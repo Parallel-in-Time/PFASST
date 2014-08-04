148fec36210a9ecea8ed699d0f0ff330b121a2ff
Matthew Emmett <mwemmett@lbl.gov>
2014-08-04 15:53:23 -0700

    interp: Flense.

--- include/pfasst/encap/poly_interp.hpp
+++ include/pfasst/encap/poly_interp.hpp
@@ -27,8 +27,9 @@ namespace pfasst
       public:
         virtual ~PolyInterpMixin() { }
 
+
         virtual void interpolate_initial(shared_ptr<ISweeper<time>> dst,
-                                         shared_ptr<const ISweeper<time>> src)
+                                         shared_ptr<const ISweeper<time>> src) override
         {
           auto& fine = as_encap_sweeper(dst);
           auto& crse = as_encap_sweeper(src);
@@ -47,9 +48,10 @@ namespace pfasst
           fine.evaluate(0);
         }
 
+
         virtual void interpolate(shared_ptr<ISweeper<time>> dst,
                                  shared_ptr<const ISweeper<time>> src,
-                                 bool interp_initial)
+                                 bool interp_initial) override
         {
           auto& fine = as_encap_sweeper(dst);
           auto& crse = as_encap_sweeper(src);
@@ -69,22 +71,11 @@ namespace pfasst
           for (size_t m = 0; m < nfine; m++) { fine_state[m] = fine.get_state(m); }
           for (size_t m = 0; m < ncrse; m++) { fine_delta[m] = fine_factory->create(solution); }
 
-          // if (interp_delta_from_initial) {
-          //   for (size_t m = 1; m < nfine; m++) {
-          //     fine_state[m]->copy(fine_state[0]);
-          //   }
-          // }
-
           auto crse_delta = crse_factory->create(solution);
           size_t m0 = interp_initial ? 0 : 1;
           for (size_t m = m0; m < ncrse; m++) {
             crse_delta->copy(crse.get_state(m));
-            // if (interp_delta_from_initial) {
-            //   crse_delta->saxpy(-1.0, crse->get_saved_state(0));
-            //   // crse_delta->saxpy(-1.0, crse->get_state(0));
-            // } else {
-              crse_delta->saxpy(-1.0, crse.get_saved_state(m));
-            // }
+            crse_delta->saxpy(-1.0, crse.get_saved_state(m));
             interpolate(fine_delta[m], crse_delta);
           }
 
@@ -97,35 +88,29 @@ namespace pfasst
           for (size_t m = m0; m < nfine; m++) { fine.evaluate(m); }
         }
 
-        virtual void restrict(shared_ptr<ISweeper<time>> dst,
-                              shared_ptr<const ISweeper<time>> src,
-                              bool restrict_initial,
-                              bool restrict_initial_only)
-        {
-          shared_ptr<EncapSweeper<time>> crse = dynamic_pointer_cast<EncapSweeper<time>>(dst);
-          assert(crse);
-          shared_ptr<const EncapSweeper<time>> fine = dynamic_pointer_cast<const EncapSweeper<time>>(src);
-          assert(fine);
 
-          this->restrict(crse, fine, restrict_initial, restrict_initial_only);
+        virtual void restrict_initial(shared_ptr<ISweeper<time>> dst,
+                                      shared_ptr<const ISweeper<time>> src) override
+        {
+          auto& crse = as_encap_sweeper(dst);
+          auto& fine = as_encap_sweeper(src);
+          this->restrict(crse.get_state(0), fine.get_state(0));
         }
 
-        virtual void restrict(shared_ptr<EncapSweeper<time>> crse,
-                              shared_ptr<const EncapSweeper<time>> fine,
-                              bool restrict_initial,
-                              bool restrict_initial_only)
+
+        virtual void restrict(shared_ptr<ISweeper<time>> dst,
+                              shared_ptr<const ISweeper<time>> src,
+                              bool restrict_initial) override
         {
-          if (restrict_initial_only) {
-            this->restrict(crse->get_state(0), fine->get_state(0));
-            return;
-          }
+          auto& crse = as_encap_sweeper(dst);
+          auto& fine = as_encap_sweeper(src);
 
-          auto dnodes = crse->get_nodes();
-          auto snodes = fine->get_nodes();
+          auto dnodes = crse.get_nodes();
+          auto snodes = fine.get_nodes();
 
-          size_t ncrse = crse->get_nodes().size();
+          size_t ncrse = crse.get_nodes().size();
           assert(ncrse > 1);
-          size_t nfine = fine->get_nodes().size();
+          size_t nfine = fine.get_nodes().size();
 
           int trat = (int(nfine) - 1) / (int(ncrse) - 1);
 
@@ -134,31 +119,24 @@ namespace pfasst
             if (dnodes[m] != snodes[m * trat]) {
               throw NotImplementedYet("coarse nodes must be nested");
             }
-            this->restrict(crse->get_state(m), fine->get_state(m * trat));
+            this->restrict(crse.get_state(m), fine.get_state(m * trat));
           }
 
-          for (size_t m = m0; m < ncrse; m++) { crse->evaluate(m); }
+          for (size_t m = m0; m < ncrse; m++) { crse.evaluate(m); }
         }
+
 
         virtual void fas(time dt, shared_ptr<ISweeper<time>> dst,
-                         shared_ptr<const ISweeper<time>> src)
+                         shared_ptr<const ISweeper<time>> src) override
         {
-          shared_ptr<EncapSweeper<time>> crse = dynamic_pointer_cast<EncapSweeper<time>>(dst);
-          assert(crse);
-          shared_ptr<const EncapSweeper<time>> fine = dynamic_pointer_cast<const EncapSweeper<time>>(src);
-          assert(fine);
+          auto& crse = as_encap_sweeper(dst);
+          auto& fine = as_encap_sweeper(src);
 
-          this->fas(dt, crse, fine);
-        }
+          size_t ncrse = crse.get_nodes().size(); assert(ncrse >= 1);
+          size_t nfine = fine.get_nodes().size(); assert(nfine >= 1);
 
-        virtual void fas(time dt, shared_ptr<EncapSweeper<time>> crse,
-                         shared_ptr<const EncapSweeper<time>> fine)
-        {
-          size_t ncrse = crse->get_nodes().size(); assert(ncrse >= 1);
-          size_t nfine = fine->get_nodes().size(); assert(nfine >= 1);
-
-          auto crse_factory = crse->get_factory();
-          auto fine_factory = fine->get_factory();
+          auto crse_factory = crse.get_factory();
+          auto fine_factory = fine.get_factory();
 
           EncapVecT crse_z2n(ncrse - 1), fine_z2n(nfine - 1), rstr_z2n(ncrse - 1);
 
@@ -167,13 +145,13 @@ namespace pfasst
           for (size_t m = 0; m < nfine - 1; m++) { fine_z2n[m] = fine_factory->create(solution); }
 
           // compute '0 to node' integral on the coarse level
-          crse->integrate(dt, crse_z2n);
+          crse.integrate(dt, crse_z2n);
           for (size_t m = 1; m < ncrse - 1; m++) {
             crse_z2n[m]->saxpy(1.0, crse_z2n[m - 1]);
           }
 
           // compute '0 to node' integral on the fine level
-          fine->integrate(dt, fine_z2n);
+          fine.integrate(dt, fine_z2n);
           for (size_t m = 1; m < nfine - 1; m++) {
             fine_z2n[m]->saxpy(1.0, fine_z2n[m - 1]);
           }
@@ -186,7 +164,7 @@ namespace pfasst
 
           // compute 'node to node' tau correction
           EncapVecT tau(ncrse - 1), rstr_and_crse(2 * (ncrse - 1));
-          for (size_t m = 0; m < ncrse - 1; m++) { tau[m] = crse->get_tau(m); }
+          for (size_t m = 0; m < ncrse - 1; m++) { tau[m] = crse.get_tau(m); }
           for (size_t m = 0; m < ncrse - 1; m++) { rstr_and_crse[m] = rstr_z2n[m]; }
           for (size_t m = 0; m < ncrse - 1; m++) { rstr_and_crse[ncrse - 1 + m] = crse_z2n[m]; }
 
