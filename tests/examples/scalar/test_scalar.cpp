b7a0579b449d4a7dd29a05d11542ead3034de187
Daniel <daniel@comp.usilu.net>
2014-08-12 10:44:32 +0200

    test_scalar: computation of convergence rate moved from TEST_P script into ConvergenceTest class ; started to add some lines for legendre node testing (not yet complete)

--- tests/examples/scalar/test_scalar.cpp
+++ tests/examples/scalar/test_scalar.cpp
@@ -27,9 +27,10 @@ class ConvergenceTest
     size_t nsteps_l;
     vector<double> err;
     vector<double> convrate;
+    vector<double> convrate_legendre;
     double dt;
     size_t niters;
-    const pfasst::QuadratureType nodetype = pfasst::QuadratureType::GaussLobatto;
+    pfasst::QuadratureType nodetype = pfasst::QuadratureType::GaussLobatto;
 
   public:
     virtual void SetUp()
@@ -38,16 +39,34 @@ class ConvergenceTest
       this->nsteps_l = this->nsteps.size();
       this->err.resize(this->nsteps.size());
       this->convrate.resize(this->nsteps.size());
+    
+      // Run first for Lobatto nodes
+
 
       // Expect convergence rate of 2*nodes-2 from collocation formula,
       // doing an identical number of iteration should suffice to reach this as each
       // iteration should increase order by one
       this->niters = 2 * nnodes - 2;
 
+      // run to compute errors
       for (size_t i = 0; i <= nsteps_l - 1; ++i) {
         dt = Tend / double(nsteps[i]);
         err[i] = run_scalar_sdc(nsteps[i], dt, nnodes, niters, lambda, nodetype);
       }
+
+      // compute convergence rates
+      for (size_t i = 0; i <= nsteps_l - 2; ++i) {
+        convrate[i] = log10(err[i+1] / err[i]) / log10(double(nsteps[i]) / double(nsteps[i + 1]));
+      }
+          
+      // Run for Legendre nodes
+      nodetype = pfasst::QuadratureType::GaussLegendre;
+      this->convrate_legendre.resize(this->nsteps.size());
+      
+      // NOTE: Setting M for Legendre nodes actually only results in M-2 "real" nodes,
+      // because the first and the last are used for initial and final value.
+      this->niters = 2*(nnodes-2);
+      
     }
 
     virtual void TearDown()
@@ -62,7 +81,6 @@ class ConvergenceTest
 TEST_P(ConvergenceTest, GaussLobattoNodes)
 {
   for (size_t i = 0; i <= nsteps_l - 2; ++i) {
-    convrate[i] = log10(err[i+1] / err[i]) / log10(double(nsteps[i]) / double(nsteps[i + 1]));
 
     EXPECT_THAT(convrate[i],
                 testing::DoubleNear(double(2 * nnodes - 2), 0.99)) << "Convergence rate at node "
