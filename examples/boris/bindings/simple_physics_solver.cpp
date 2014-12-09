af9117e6d354d656a7802dec955783358f1352a9
Torbjörn Klatt <t.klatt@fz-juelich.de>
2014-12-09 14:58:39 +0100

    examples: boris: some tweaks to example solver library

--- examples/boris/bindings/simple_physics_solver.cpp
+++ examples/boris/bindings/simple_physics_solver.cpp
@@ -8,167 +8,176 @@ using namespace std;
 
 namespace simple_physics_solver
 {
-  template<typename scalar, typename time>
-  SimplePhysicsSolverConfig<scalar, time>::SimplePhysicsSolverConfig(const scalar omega_e, const scalar omega_b,
-                                                                     const scalar epsilon, const scalar sigma)
+  SimplePhysicsSolverConfig::SimplePhysicsSolverConfig(const double omega_e, const double omega_b,
+                                                       const double epsilon, const double sigma)
     :   omega_e(omega_e)
       , omega_b(omega_b)
       , epsilon(epsilon)
       , sigma(sigma)
       , sigma2(sigma * sigma)
   {
-    this->external_e_field_matrix[0][0] = scalar(1.0);
-    this->external_e_field_matrix[0][1] = scalar(0.0);
-    this->external_e_field_matrix[0][2] = scalar(0.0);
-    this->external_e_field_matrix[1][0] = scalar(0.0);
-    this->external_e_field_matrix[1][1] = scalar(1.0);
-    this->external_e_field_matrix[1][2] = scalar(0.0);
-    this->external_e_field_matrix[2][0] = scalar(0.0);
-    this->external_e_field_matrix[2][1] = scalar(0.0);
-    this->external_e_field_matrix[2][2] = scalar(-2.0);
+    this->external_e_field_matrix[0][0] = double(1.0);
+    this->external_e_field_matrix[0][1] = double(0.0);
+    this->external_e_field_matrix[0][2] = double(0.0);
+    this->external_e_field_matrix[1][0] = double(0.0);
+    this->external_e_field_matrix[1][1] = double(1.0);
+    this->external_e_field_matrix[1][2] = double(0.0);
+    this->external_e_field_matrix[2][0] = double(0.0);
+    this->external_e_field_matrix[2][1] = double(0.0);
+    this->external_e_field_matrix[2][2] = double(-2.0);
 
-    this->b_field_matrix[0][0] = scalar(0.0);
-    this->b_field_matrix[0][1] = scalar(1.0);
-    this->b_field_matrix[0][2] = scalar(0.0);
-    this->b_field_matrix[1][0] = scalar(-1.0);
-    this->b_field_matrix[1][1] = scalar(0.0);
-    this->b_field_matrix[1][2] = scalar(0.0);
-    this->b_field_matrix[2][0] = scalar(0.0);
-    this->b_field_matrix[2][1] = scalar(0.0);
-    this->b_field_matrix[2][2] = scalar(0.0);
+    this->b_field_matrix[0][0] = double(0.0);
+    this->b_field_matrix[0][1] = double(1.0);
+    this->b_field_matrix[0][2] = double(0.0);
+    this->b_field_matrix[1][0] = double(-1.0);
+    this->b_field_matrix[1][1] = double(0.0);
+    this->b_field_matrix[1][2] = double(0.0);
+    this->b_field_matrix[2][0] = double(0.0);
+    this->b_field_matrix[2][1] = double(0.0);
+    this->b_field_matrix[2][2] = double(0.0);
   }
 
-  template<typename scalar, typename time>
-  SimplePhysicsSolverConfig<scalar, time>::~SimplePhysicsSolverConfig()
+  SimplePhysicsSolverConfig::~SimplePhysicsSolverConfig()
   {}
 
 
-  template<typename scalar, typename time>
-  static void evaluate_external_e_field(const scalar** positions, const scalar* charges, const scalar* masses,
-                                        const size_t num_particles, const size_t dim, const time t,
-                                        const SimplePhysicsSolverConfig<scalar, time>& config,
-                                        scalar** forces)
+  void evaluate_external_e_field(const double* positions, const double* charges, const double* masses,
+                                 const size_t num_particles, const double t,
+                                 const SimplePhysicsSolverConfig* config,
+                                 double* forces)
   {
     UNUSED(t);
-    scalar pre_factor = (- config.epsilon) * (config.omega_e * config.omega_e);
-    scalar factor;
+    double pre_factor = (- config->epsilon) * (config->omega_e * config->omega_e);
+    double factor = double(0.0);
     for (size_t i = 0; i < num_particles; ++i) {
       factor = pre_factor / (charges[i] / masses[i]);
-      internal::scale_mat_mul_vec(config.external_e_field_matrix, positions[i], dim, dim, factor, forces[i]);
+      internal::scale_mat_mul_vec(config->external_e_field_matrix, positions + i, factor, forces + i);
     }
   }
 
-  template<typename scalar, typename time>
-  static void evaluate_internal_e_field(const scalar** positions, const scalar* charges, const scalar* masses,
-                                        const size_t num_particles, const size_t dim, const time t,
-                                        const SimplePhysicsSolverConfig<scalar, time>& config,
-                                        scalar** exyz, scalar* phis)
+  void evaluate_internal_e_field(const double* positions, const double* charges, const double* masses,
+                                 const size_t num_particles, const double t,
+                                 const SimplePhysicsSolverConfig* config,
+                                 double* exyz, double* phis)
   {
     UNUSED(masses); UNUSED(t);
-    scalar r = scalar(0.0),
-           r3 = scalar(0.0),
-           dist = scalar(0.0);
+    double r = double(0.0),
+           r3 = double(0.0),
+           dist = double(0.0);
 
     // computing forces on particle i
     for (size_t i = 0; i < num_particles; ++i) {
 
       // null result values
-      phis[i] = scalar(0.0);
-      for (size_t d = 0; d < dim; ++d) { exyz[i][d] = scalar(0.0); }
+      phis[i] = double(0.0);
+      for (size_t d = 0; d < DIM; ++d) { exyz[i * DIM + d] = double(0.0); }
 
       // effects of particle j on particle i
       for (size_t j = 0; j < num_particles; ++j) {
-        dist = scalar(0.0);
-        for (size_t d = 0; d < dim; ++d) {
-          dist += (positions[i * dim + d] - positions[j * dim + d]) * (positions[i * dim + d] - positions[j * dim + d]);
+        dist = double(0.0);
+        for (size_t d = 0; d < DIM; ++d) {
+          dist += (positions[i * DIM + d] - positions[j * DIM + d]) * (positions[i * DIM + d] - positions[j * DIM + d]);
         }
-        r = sqrt(dist * dist + config.sigma2);
+        r = sqrt(dist * dist + config->sigma2);
         phis[i] += charges[j] / r;
 
         r3 = r * r * r;
-        for (size_t d = 0; d < dim; ++d) {
-          exyz[i][d] += positions[j][d] / r3 * charges[j];
+        for (size_t d = 0; d < DIM; ++d) {
+          exyz[i * DIM + d] += positions[j * DIM + d] / r3 * charges[j];
         }
       }
     }
   }
 
 
-  template<typename scalar, typename time>
-  static void get_b_field_vector(const SimplePhysicsSolverConfig<scalar, time>& config, const size_t dim,
-                                 scalar* b_field_vector)
+  void evaluate_e_field(const double* positions, const double* charges, const double* masses,
+                        const size_t num_particles, const double t,
+                        const SimplePhysicsSolverConfig* config,
+                        double* forces)
   {
-    for (size_t i = 0; i < dim; ++i) {
-      b_field_vector[i] = time(0.0);
+    double* external = new double[num_particles * DIM];
+    double* internal = new double[num_particles * DIM];
+    double* phis = new double[num_particles];
+    evaluate_external_e_field(positions, charges, masses, num_particles, t, config, external);
+    evaluate_internal_e_field(positions, charges, masses, num_particles, t, config, internal, phis);
+    for (size_t i = 0; i < num_particles; ++i) {
+      for (size_t d = 0; d < DIM; ++d) {
+        forces[i * DIM + d] = external[i * DIM + d] + internal[i * DIM + d];
+      }
     }
-    b_field_vector[2] = time(1.0);
-    for (size_t i = 0; i < dim; ++i) {
-      b_field_vector[i] *= config.omega_b;
+    delete[] external;
+    delete[] internal;
+    delete[] phis;
+  }
+
+
+  void get_b_field_vector(const SimplePhysicsSolverConfig* config, double* b_field_vector)
+  {
+    for (size_t i = 0; i < DIM; ++i) {
+      b_field_vector[i] = double(0.0);
+    }
+    b_field_vector[2] = double(1.0);
+    for (size_t i = 0; i < DIM; ++i) {
+      b_field_vector[i] *= config->omega_b;
     }
   }
 
-  template<typename scalar, typename time>
-  static void evaluate_b_field(const scalar** velocities, const scalar* masses, const scalar* charges,
-                               const size_t num_particles, const size_t dim, const time t,
-                               const SimplePhysicsSolverConfig<scalar, time>& config, scalar** forces)
+  void evaluate_b_field(const double* velocities, const double* charges, const double* masses,
+                        const size_t num_particles, const double t,
+                        const SimplePhysicsSolverConfig* config, double* forces)
   {
     UNUSED(t);
     for (size_t i = 0; i < num_particles; ++i) {
-      internal::scale_mat_mul_vec(config.b_field_matrix, velocities[i], dim, dim,
-                                  config.omega_b / (charges[i] / masses[i]),
-                                  forces[i]);
+      internal::scale_mat_mul_vec(config->b_field_matrix, velocities + (i * DIM),
+                                  config->omega_b / (charges[i] / masses[i]),
+                                  forces + (i * DIM));
     }
   }
 
 
-  template<typename scalar, typename time>
-  static scalar compute_energy(const scalar** positions, const scalar** velocities, const scalar* masses,
-                               const scalar* charges,
-                               const size_t num_particles, const size_t dim, const time t,
-                               const SimplePhysicsSolverConfig<scalar, time>& config)
+  double compute_energy(const double* positions, const double* velocities,
+                        const double* charges, const double* masses,
+                        const size_t num_particles, const double t,
+                        const SimplePhysicsSolverConfig* config)
   {
-    assert(dim == 3);
-    scalar e_kin = scalar(0.0);
-    scalar e_pot = scalar(0.0);
+    double e_kin = double(0.0);
+    double e_pot = double(0.0);
 
-    scalar phi_i = scalar(0.0),
-           r = scalar(0.0),
-           dist = scalar(0.0);
-    scalar cross_prod[dim];
+    double cross_prod[DIM];
 
-    scalar exyz[num_particles][dim];
-    scalar phis[num_particles];
+    double* exyz = new double[num_particles * DIM];
+    double* phis = new double[num_particles];
 
-    evaluate_internal_e_field(positions, charges, masses, num_particles, dim, t, config, exyz, phis);
+    evaluate_internal_e_field(positions, charges, masses, num_particles, t, config, exyz, phis);
 
     for (size_t i = 0; i < num_particles; ++i) {
       e_pot += charges[i] * phis[i];
-      internal::cross_prod(velocities[i], velocities[i], cross_prod);
-      for (size_t m = 0; m < dim; ++m) {
-        e_kin += masses[i] / scalar(2.0) * cross_prod[m];
+      internal::cross_prod(velocities + (i * DIM), velocities + (i * DIM), cross_prod);
+      for (size_t m = 0; m < DIM; ++m) {
+        e_kin += masses[i] / double(2.0) * cross_prod[m];
       }
     }
+    delete[] exyz;
+    delete[] phis;
 
     return e_kin + e_pot;
   }
 
   namespace internal
   {
-    template<typename scalar>
-    static void cross_prod(const scalar* first, const scalar* second, scalar* cross_prod)
+    static void cross_prod(const double* first, const double* second, double* cross_prod)
     {
       cross_prod[0] = first[1] * second[2] - first[2] * second[1];
       cross_prod[1] = first[2] * second[0] - first[0] * second[2];
       cross_prod[2] = first[0] * second[1] - first[1] * second[0];
     }
 
-    template<typename scalar>
-    static void scale_mat_mul_vec(const scalar** mat, const scalar* vec, const size_t rows, const size_t cols,
-                                  const scalar factor, scalar* prod)
+    static void scale_mat_mul_vec(const double mat[DIM][DIM], const double vec[DIM],
+                                  const double factor, double* prod)
     {
-      for (size_t i = 0; i < rows; ++i) {
-        prod[i] = scalar(0.0);
-        for (size_t j = 0; j < cols; ++j) {
+      for (size_t i = 0; i < DIM; ++i) {
+        prod[i] = double(0.0);
+        for (size_t j = 0; j < DIM; ++j) {
           prod[i] += factor * mat[i][j] * vec[j];
         }
       }
--- examples/boris/bindings/simple_physics_solver.hpp
+++ examples/boris/bindings/simple_physics_solver.hpp
@@ -4,64 +4,61 @@
 #include <cstdlib>
 using namespace std;
 
+#ifndef DIM
+  #define DIM 3
+#endif
+
 namespace simple_physics_solver
 {
-  template<
-    typename scalar,
-    typename time
-  >
   class SimplePhysicsSolverConfig
   {
     public:
-      scalar omega_e;
-      scalar omega_b;
-      scalar epsilon;
-      scalar sigma;
-      scalar sigma2;
-      scalar external_e_field_matrix[3][3];
-      scalar b_field_matrix[3][3];
+      double omega_e;
+      double omega_b;
+      double epsilon;
+      double sigma;
+      double sigma2;
+      double external_e_field_matrix[3][3];
+      double b_field_matrix[3][3];
 
-      SimplePhysicsSolverConfig(const scalar omega_e = scalar(4.9), const scalar omega_b = scalar(25.0),
-                                const scalar epsilon = scalar(-1.0), const scalar sigma = scalar(0.1));
+      SimplePhysicsSolverConfig(const double omega_e = double(4.9), const double omega_b = double(25.0),
+                                const double epsilon = double(-1.0), const double sigma = double(0.1));
       virtual ~SimplePhysicsSolverConfig();
   };
 
-  template<typename scalar, typename time>
-  static void evaluate_external_e_field(const scalar** positions, const scalar* charges, const scalar* masses,
-                                        const size_t num_particles, const size_t dim, const time t,
-                                        const SimplePhysicsSolverConfig<scalar, time>& config,
-                                        scalar** forces);
+  void evaluate_external_e_field(const double* positions, const double* charges, const double* masses,
+                                 const size_t num_particles, const double t,
+                                 const SimplePhysicsSolverConfig* config,
+                                 double* forces);
 
-  template<typename scalar, typename time>
-  static void evaluate_internal_e_field(const scalar** positions, const scalar* charges, const scalar* masses,
-                                        const size_t num_particles, const size_t dim, const time t,
-                                        const SimplePhysicsSolverConfig<scalar, time>& config,
-                                        scalar** exyz, scalar* phis);
+  void evaluate_internal_e_field(const double* positions, const double* charges, const double* masses,
+                                 const size_t num_particles, const double t,
+                                 const SimplePhysicsSolverConfig* config,
+                                 double* exyz, double* phis);
+  
+  void evaluate_e_field(const double* positions, const double* charges, const double* masses,
+                        const size_t num_particles, const double t,
+                        const SimplePhysicsSolverConfig* config,
+                        double* forces);
 
-  template<typename scalar, typename time>
-  static void get_b_field_vector(const SimplePhysicsSolverConfig<scalar, time>& config, const size_t dim,
-                                 scalar* b_field_vector);
+  void get_b_field_vector(const SimplePhysicsSolverConfig* config,
+                                 double* b_field_vector);
 
-  template<typename scalar, typename time>
-  static void evaluate_b_field(const scalar** velocities, const scalar* masses, const scalar* charges,
-                               const size_t num_particles, const size_t dim, const time t,
-                               const SimplePhysicsSolverConfig<scalar, time>& config,
-                               scalar** forces);
+  void evaluate_b_field(const double* velocities, const double* masses, const double* charges,
+                        const size_t num_particles, const double t,
+                        const SimplePhysicsSolverConfig* config,
+                        double* forces);
 
-  template<typename scalar, typename time>
-  static scalar compute_energy(const scalar** positions, const scalar** velocities, const scalar* masses,
-                               const scalar* charges,
-                               const size_t num_particles, const size_t dim, const time t,
-                               const SimplePhysicsSolverConfig<scalar, time>& config);
+  double compute_energy(const double* positions, const double* velocities, const double* masses,
+                        const double* charges,
+                        const size_t num_particles, const double t,
+                        const SimplePhysicsSolverConfig* config);
 
   namespace internal
   {
-    template<typename scalar>
-    static void cross_prod(const scalar* first, const scalar* second, scalar* cross_prod);
+    static void cross_prod(const double* first, const double* second, double* cross_prod);
 
-    template<typename scalar>
-    static void scale_mat_mul_vec(const scalar** mat, const scalar* vec, const size_t rows, const size_t cols,
-                                  const scalar factor, scalar* prod);
+    static void scale_mat_mul_vec(const double mat[DIM][DIM], const double vec[DIM], const double factor, double* prod);
   }  // ::simple_physics_solver::internal
 }  // ::simple_physics_solver
 
