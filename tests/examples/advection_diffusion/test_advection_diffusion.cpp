0ebff470205e1a4fc09bc09b761bbe0d28909ddd
Matthew Emmett <memmett@gmail.com>
2015-01-21 13:22:53 -0800

    tests: Re-organise the adaptive MLSDC tests a bit.
    Signed-off-by: Matthew Emmett <memmett@gmail.com>

--- tests/examples/advection_diffusion/test_advection_diffusion.cpp
+++ tests/examples/advection_diffusion/test_advection_diffusion.cpp
@@ -35,39 +35,43 @@ TEST(ErrorTest, VanillaSDC)
   auto get_iter  = [](const vtype x) { return get<1>(get<0>(x)); };
   auto get_error = [](const vtype x) { return get<1>(x); };
 
-  {
-    auto errors = run_vanilla_sdc(0.0);
-    auto max_iter = get_iter(*std::max_element(errors.begin(), errors.end(),
-                                               [get_iter](const vtype p1, const vtype p2) { return get_iter(p1) < get_iter(p2); }));
+  auto errors = run_vanilla_sdc(0.0);
+  auto max_iter = get_iter(*std::max_element(errors.begin(), errors.end(),
+                                             [get_iter](const vtype p1, const vtype p2) { return get_iter(p1) < get_iter(p2); }));
 
-    vector<double> tol = { 7e-9, 7e-9, 7e-9, 7e-9 };
-    vector<double> err;
-    for (auto& x: errors) {
-      if (get_iter(x) == max_iter) {
-        err.push_back(get_error(x));
-      }
+  vector<double> tol = { 7e-9, 7e-9, 7e-9, 7e-9 };
+  vector<double> err;
+  for (auto& x: errors) {
+    if (get_iter(x) == max_iter) {
+      err.push_back(get_error(x));
     }
-
-    EXPECT_THAT(err, testing::Pointwise(DoubleLess(), tol));
-    ASSERT_EQ(max_iter, (size_t)3);
   }
 
-  {
-    auto errors = run_vanilla_sdc(1.e-6);
-    auto max_iter = get_iter(*std::max_element(errors.begin(), errors.end(),
-                                               [get_iter](const vtype p1, const vtype p2) { return get_iter(p1) < get_iter(p2); }));
+  EXPECT_THAT(err, testing::Pointwise(DoubleLess(), tol));
+  ASSERT_EQ(max_iter, (size_t) 3);
+}
 
-    vector<double> tol = { 5e-8, 5e-8, 5e-8, 5e-8 };
-    vector<double> err;
-    for (auto& x: errors) {
-      if (get_iter(x) == max_iter) {
-        err.push_back(get_error(x));
-      }
+TEST(AdaptiveErrorTest, VanillaSDC)
+{
+  typedef error_map::value_type vtype;
+
+  auto get_iter  = [](const vtype x) { return get<1>(get<0>(x)); };
+  auto get_error = [](const vtype x) { return get<1>(x); };
+
+  auto errors = run_vanilla_sdc(1.e-6);
+  auto max_iter = get_iter(*std::max_element(errors.begin(), errors.end(),
+                                             [get_iter](const vtype p1, const vtype p2) { return get_iter(p1) < get_iter(p2); }));
+
+  vector<double> tol = { 5e-8, 5e-8, 5e-8, 5e-8 };
+  vector<double> err;
+  for (auto& x: errors) {
+    if (get_iter(x) == max_iter) {
+      err.push_back(get_error(x));
     }
-
-    EXPECT_THAT(err, testing::Pointwise(DoubleLess(), tol));
-    ASSERT_EQ(max_iter, (size_t)2);
   }
+
+  EXPECT_THAT(err, testing::Pointwise(DoubleLess(), tol));
+  ASSERT_EQ(max_iter, (size_t) 2);
 }
 
 TEST(ErrorTest, SerialMLSDC)
