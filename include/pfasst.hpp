1bd27f271fa7a4925cc610516479fb6582e2ebd3
Torbjörn Klatt <t.klatt@fz-juelich.de>
2015-01-28 11:44:20 +0100

    config: enable cmd args for residual tolerances

--- include/pfasst.hpp
+++ include/pfasst.hpp
@@ -6,12 +6,14 @@
 #include "pfasst/interfaces.hpp"
 #include "pfasst/quadrature.hpp"
 #include "pfasst/sdc.hpp"
+#include "pfasst/encap/encap_sweeper.hpp"
 
 namespace pfasst
 {
   inline static void init(int argc, char** argv)
   {
     SDC<>::enable_config_options(0);
+    encap::EncapSweeper<>::enable_config_options(0);
     Quadrature::enable_config_options(0);
     config::init_config();
     log::start_log(argc, argv);
