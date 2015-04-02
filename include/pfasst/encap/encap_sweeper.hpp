d04f309e41cab0a8dcf10acf45ab2c05e7747c6b
Torbjörn Klatt <t.klatt@fz-juelich.de>
2015-04-02 19:50:48 +0200

    docu: improve Encapsulation and EncapSweeper

--- include/pfasst/encap/encap_sweeper.hpp
+++ include/pfasst/encap/encap_sweeper.hpp
@@ -1,7 +1,7 @@
-/*
- * Host based encapsulated base sweeper.
+/**
+ * @file pfasst/encap/encap_sweeper.hpp
+ * @since v0.1.0
  */
-
 #ifndef _PFASST_ENCAP_ENCAP_SWEEPER_HPP_
 #define _PFASST_ENCAP_ENCAP_SWEEPER_HPP_
 
@@ -20,59 +20,105 @@ namespace pfasst
   {
     using namespace pfasst::quadrature;
 
+
+    /**
+     * host based encapsulated base sweeper.
+     *
+     * @tparam time time precision; defaults to pfasst::time_precision
+     */
     template<typename time = time_precision>
     class EncapSweeper
       : public ISweeper<time>
     {
       protected:
         //! @{
+        //! quadrature rule used by this sweeper
         shared_ptr<IQuadrature<time>> quadrature;
+
+        //! encapsulation data structure factory
         shared_ptr<EncapFactory<time>> factory;
+
+        //! separate start state, i.e. initial condition for the sweeper's current time step
         shared_ptr<Encapsulation<time>> start_state;
+
+        //! current solution at \\( T_{end} \\)
         shared_ptr<Encapsulation<time>> end_state;
+
+        /**
+         * place for the residuals at the different time nodes
+         *
+         * The index of the vector corresponds to the index of the quadrature nodes, i.e.
+         * `residuals.size() == quadrature->get_num_nodes()`.
+         */
         vector<shared_ptr<Encapsulation<time>>> residuals;
         //! @}
 
         //! @{
         /**
-         * Solution values \\( U \\) at all time nodes of the current iteration.
+         * solution values \\( U \\) at all time nodes of the current iteration.
+         *
+         * The index of the vector corresponds to the index of the quadrature nodes, i.e.
+         * `state.size() == quadrature->get_num_nodes()`.
          */
         vector<shared_ptr<Encapsulation<time>>> state;
 
         /**
-         * Solution values \\( U \\) at all time nodes of the previous iteration.
+         * solution values \\( U \\) at all time nodes of the previous iteration.
+         *
+         * The index of the vector corresponds to the index of the quadrature nodes, i.e.
+         * `saved_state.size() == quadrature->get_num_nodes()`.
          */
         vector<shared_ptr<Encapsulation<time>>> saved_state;
 
         /**
          * FAS corrections \\( \\tau \\) at all time nodes of the current iteration.
+         *
+         * The index of the vector corresponds to the index of the quadrature nodes, i.e.
+         * `fas_corrections.size() == quadrature->get_num_nodes()`.
          */
         vector<shared_ptr<Encapsulation<time>>> fas_corrections;
         //! @}
 
+        //! @{
+        //! @todo Write documentation for this member.
         int residual_norm_order;
-        time abs_residual_tol, rel_residual_tol;
+
+        /**
+         * tolerance for absolute residual.
+         *
+         * The absolute residual is the residual between the very first iteration and the current
+         * state.
+         */
+        time abs_residual_tol;
+
+        /**
+         * tolerance for relative residual.
+         *
+         * The relative residual is the residual between the previous iteration and current state.
+         */
+        time rel_residual_tol;
+        //! @}
 
       public:
         EncapSweeper();
 
         //! @{
         /**
-         * Retrieve solution values of current iteration at time node index `m`.
+         * retrieve solution values of current iteration at time node index @p m.
          *
          * @param[in] m 0-based index of time node
          */
         virtual shared_ptr<Encapsulation<time>> get_state(size_t m) const;
 
         /**
-         * Retrieve FAS correction of current iteration at time node index `m`.
+         * retrieve FAS correction of current iteration at time node index @p m.
          *
          * @param[in] m 0-based index of time node
          */
         virtual shared_ptr<Encapsulation<time>> get_tau(size_t m) const;
 
         /**
-         * Retrieve solution values of previous iteration at time node index `m`.
+         * retrieve solution values of previous iteration at time node index @p m.
          *
          * @param[in] m 0-based index of time node
          */
@@ -80,15 +126,25 @@ namespace pfasst
         //! @}
 
         //! @{
+        /**
+         * @copybrief ISweeper::set_options()
+         */
         virtual void set_options() override;
+
+        /**
+         * @copybrief ISweeper::setup()
+         */
         virtual void setup(bool coarse) override;
         //! @}
 
         //! @{
+        /**
+         * @copybrief ISweeper::spread()
+         */
         virtual void spread() override;
 
         /**
-         * Save current solution states.
+         * @copybrief ISweeper::save()
          */
         virtual void save(bool initial_only) override;
         //! @}
@@ -96,25 +152,27 @@ namespace pfasst
         //! @{
         virtual void set_quadrature(shared_ptr<IQuadrature<time>> quadrature);
         virtual shared_ptr<const IQuadrature<time>> get_quadrature() const;
-        virtual shared_ptr<Encapsulation<time>> get_start_state() const;
+
         virtual const vector<time> get_nodes() const;
+
         virtual void set_factory(shared_ptr<EncapFactory<time>> factory);
         virtual shared_ptr<EncapFactory<time>> get_factory() const;
+
+        virtual shared_ptr<Encapsulation<time>> get_start_state() const;
         virtual shared_ptr<Encapsulation<time>> get_end_state();
         //! @}
 
         //! @{
         /**
-         * @copydoc ISweeper::advance()
-         *
-         * @note This method must be implemented in derived sweepers.
+         * @copybrief ISweeper::advance()
          */
         virtual void advance() override;
 
         /**
-         * Re-evaluate function values.
+         * re-evaluate function values.
          *
-         * @note This method must be implemented in derived sweepers.
+         * @param[in] initial_only whether the right hand side should only be evaluated at the
+         *   initial time point
          */
         virtual void reevaluate(bool initial_only = false);
 
@@ -122,37 +180,55 @@ namespace pfasst
          * integrates values of right hand side at all time nodes \\( t \\in [0,M-1] \\)
          * simultaneously
          *
-         * @param[in] dt width of the time interval to integrate
+         * @param[in]     dt  width of the time interval to integrate
          * @param[in,out] dst integrated values
-         *
-         * @note This method must be implemented in derived sweepers.
          */
         virtual void integrate(time dt, vector<shared_ptr<Encapsulation<time>>> dst) const;
         //! @}
 
         //! @{
         /**
-         * Set residual tolerances for convergence checking.
+         * set residual tolerances for convergence checking.
+         *
+         * @param[in] abs_residual_tol tolerance for the absolute residual
+         * @param[in] rel_residual_tol tolerance for the relative residual
+         * @param[in] order
          */
         void set_residual_tolerances(time abs_residual_tol, time rel_residual_tol, int order = 0);
 
         /**
-         * Compute residual at each SDC node (including FAS corrections).
+         * compute residual at each SDC node (including FAS corrections).
+         *
+         * @param[in]     dt  width of the time interval to compute the residual for
+         * @param[in,out] dst place to store the residuals at time nodes
          */
         virtual void residual(time dt, vector<shared_ptr<Encapsulation<time>>> dst) const;
 
         /**
-         * Return convergence status.
-         *
-         * This is used by controllers to shortcircuit iterations.
+         * @copybrief ISweeper::converged()
          */
         virtual bool converged() override;
         //! @}
 
         //! @{
+        /**
+         * @copybrief ISweeper::post()
+         */
         virtual void post(ICommunicator* comm, int tag) override;
+
+        /**
+         * @copybrief ISweeper::send()
+         */
         virtual void send(ICommunicator* comm, int tag, bool blocking) override;
+
+        /**
+         * @copybrief ISweeper::recv()
+         */
         virtual void recv(ICommunicator* comm, int tag, bool blocking) override;
+
+        /**
+         * @copybrief ISweeper::broadcast()
+         */
         virtual void broadcast(ICommunicator* comm) override;
         //! @}
     };
--- include/pfasst/encap/encapsulation.hpp
+++ include/pfasst/encap/encapsulation.hpp
@@ -1,3 +1,7 @@
+/**
+ * @file pfasst/encap/encapsulation.hpp
+ * @since v0.1.0
+ */
 #ifndef _PFASST_ENCAPSULATED_HPP_
 #define _PFASST_ENCAPSULATED_HPP_
 
@@ -15,17 +19,23 @@ using Matrix = Eigen::Matrix<scalar, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowM
 
 namespace pfasst
 {
+  /**
+   * Encapsulations (short _encaps_) are the central data type for all PFASST++ algorithms.
+   * Encaps are both representing the unknown variable as well as the result of evaluating the
+   * right hand side of the problem equation(s).
+   */
   namespace encap
   {
+    //! @todo document EncapType; especially its intention and expected usage
     typedef enum EncapType { solution, function } EncapType;
 
     /**
-     * Data/solution encapsulation.
+     * data/solution encapsulation.
      *
      * An Encapsulation provides basic functionality of the user data used by PFASST such as
-     * mathematical operation @em axpy \\(y=ax+y\\) and packing/unpacking for message passing.
-     * @tparam time time precision
-     *     defaults to pfasst::time_precision
+     * mathematical operation _axpy_ \\( y=ax+y \\) and packing/unpacking for message passing.
+     *
+     * @tparam time time precision; defaults to pfasst::time_precision
      */
     template<typename time = time_precision>
     class Encapsulation
@@ -36,47 +46,127 @@ namespace pfasst
         //! @}
 
         //! @{
-        // required for time-parallel communications
-        virtual void post(ICommunicator* comm, int tag);
-        virtual void send(ICommunicator* comm, int tag, bool blocking);
-        virtual void recv(ICommunicator* comm, int tag, bool blocking);
-        virtual void broadcast(ICommunicator* comm);
-        //! @}
-
-        //! @{
         // required for host based encap helpers
+        /**
+         * zeroes out all values of this data structure.
+         */
         virtual void zero();
-        virtual void copy(shared_ptr<const Encapsulation<time>>);
-        //! @}
 
-        virtual time norm0() const;
+        /**
+         * copies values from @p other into this data structure.
+         *
+         * @param[in] other other data structure to copy data from
+         */
+        virtual void copy(shared_ptr<const Encapsulation<time>> other);
+        //! @}
 
         //! @{
         /**
-         * provides basic mathematical operation \\(y+=ax\\).
+         * computes the \\( 0 \\)-norm of the data structure's values.
+         *
+         * @returns \\( 0 \\)-norm of this data structure
+         */
+        virtual time norm0() const;
+        //! @}
+
+        //! @{
+        /**
+         * provides basic mathematical operation \\( y+=ax \\).
          *
          * This is the main mathematical operation applied by PFASST on the data structures.
-         * Here, \\(a\\) is a time point and \\(x\\) another data structure (usually of the
-         * same type).
+         * Here, \\( a \\) is a time point and \\( x \\) another data structure (usually of the
+         * same type) and \\( y \\) is this data structure.
+         *
+         * @param[in] a time point to multiply
+         * @param[in] x another data structure to scale-add onto this one
          */
         virtual void saxpy(time a, shared_ptr<const Encapsulation<time>> x);
 
         /**
          * defines matrix-vector multiplication for this data type.
+         *
+         * This implements the matrix-vector multiplication of the form
+         * \\( \\vec{y}=a M \\vec{x} \\).
+         * Here, \\( a \\) is a time point to scale the matrix-vector multiplication with, \\( M \\)
+         * is a matrix and \\( x \\) another data structure usually of the same type as this one.
+         *
+         * If @p zero is `true`, then @p dst is zeroed out before applying the matrix-vector
+         * multiplication.
+         * Elsewise the result of the matrix-vector multiplication is added onto @p dst.
+         *
+         * @param[in,out] dst  data structure to store the result in
+         * @param[in]     a
+         * @param[in]     mat
+         * @param[in]     src
+         * @param[in]     zero
          */
         virtual void mat_apply(vector<shared_ptr<Encapsulation<time>>> dst,
                                time a, Matrix<time> mat,
                                vector<shared_ptr<Encapsulation<time>>> src,
                                bool zero = true);
         //! @}
+
+        //! @{
+        // required for time-parallel communications
+        /**
+         * @todo Write documentation for this member function. How is it different to post/recv?
+         *
+         * @param[in] comm     communicator managing the processes to post ??? to/from ???
+         * @param[in] tag      tag to distinguish overlapping communication
+         */
+        virtual void post(ICommunicator* comm, int tag);
+
+        /**
+         * send values stored in this data structure.
+         *
+         * @param[in] comm     communicator managing the processes to send to
+         * @param[in] tag      tag to distinguish overlapping communication
+         * @param[in] blocking whether to use blocking or non-blocking send
+         */
+        virtual void send(ICommunicator* comm, int tag, bool blocking);
+
+        /**
+         * receive values to store in this data structure.
+         *
+         * @param[in] comm     communicator managing the processes to receive from
+         * @param[in] tag      tag to distinguish overlapping communication
+         * @param[in] blocking whether to use blocking or non-blocking receive
+         */
+        virtual void recv(ICommunicator* comm, int tag, bool blocking);
+
+        /**
+         * broadcasting this data structure to all processes in @p comm.
+         *
+         * @param[in] comm communicator managing the processes to send this data structure to
+         */
+        virtual void broadcast(ICommunicator* comm);
+        //! @}
     };
 
 
+    /**
+     * abstract interface of factory for creating Encapsulation objects.
+     *
+     * This factory is intendet to be instantiated once to create multiple Encapsulation objects
+     * of the same type and with the same parameters later on through calls to
+     * EncapFactory::create().
+     *
+     * Implementations may add values and parameters to the constructor of the factory storing these
+     * values as an instance member and accessing them within their implementations of create().
+     * For an example see pfasst::encap::VectorFactory.
+     *
+     * @tparam time time precision; defaults to pfasst::time_precision
+     */
     template<typename time = time_precision>
     class EncapFactory
     {
       public:
-        virtual shared_ptr<Encapsulation<time>> create(const EncapType) = 0;
+        /**
+         * actual method to create Encapsulation object of specific type
+         *
+         * @param[in] type encapsulation type of the requested Encapsulation object
+         */
+        virtual shared_ptr<Encapsulation<time>> create(const EncapType type) = 0;
     };
   }  // ::pfasst::encap
 } // ::pfasst
