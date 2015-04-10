267df67278310d003c418da53eeafbea11345350
Torbjörn Klatt <t.klatt@fz-juelich.de>
2015-04-10 10:38:01 +0200

    config: mpi: let only rank 0 print help message

--- include/pfasst/config.hpp
+++ include/pfasst/config.hpp
@@ -10,6 +10,10 @@ using namespace std;
 #include <boost/program_options.hpp>
 namespace po = boost::program_options;
 
+#ifdef WITH_MPI
+  #include <mpi.h>
+#endif
+
 
 namespace pfasst
 {
@@ -69,9 +73,27 @@ namespace pfasst
       po::store(parsed, options::get_instance().get_variables_map());
       po::notify(options::get_instance().get_variables_map());
 
+#ifdef WITH_MPI
+      int initialized = 0;
+      MPI_Initialized(&initialized);
+      assert((bool)initialized);
+      int rank = 0;
+      MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+#endif
       if (options::get_instance().get_variables_map().count("help")) {
-        cout << print_help() << endl;
-        if (exit_on_help) exit(0);
+#ifdef WITH_MPI
+        if (rank == 0) {
+#endif
+          cout << print_help() << endl;
+#ifdef WITH_MPI
+        }
+#endif
+        if (exit_on_help) {
+#ifdef WITH_MPI
+          MPI_Finalize();
+#endif
+          exit(0);
+        }
       }
     }
 
